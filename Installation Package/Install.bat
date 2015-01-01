b48d84b0c550d01271953101ce6bf2b908208ef0
Adam Wiley <addw101@gmail.com>
2015-01-01 11:35:44 -0500

    Added: JSpan_Man skins Changed: Installer package batch file has proper file extensions now

--- Installation Package/Install.bat
+++ Installation Package/Install.bat
@@ -2,5 +2,5 @@
 cd installers
 python-2.7.6.msi
 pygame-1.9.1.win32-py2.7.msi
-zope.interface-4.1.0.win32-py2.7
-Twisted-13.2.0.win32-py2.7
+zope.interface-4.1.0.win32-py2.7.exe
+Twisted-13.2.0.win32-py2.7.msi
